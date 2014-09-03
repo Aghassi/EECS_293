02199aff96095918da5061d839348e0d90496586
David <david.aghassi@gmail.com>
2014-09-03 08:44:39 -0400

    Logic implemented, front end done
    Program is mostly working, but MaskFormatter is allowing all or no
    characters.

--- 293 Programming Project 1/src/Controllers/inputReader.java
+++ 293 Programming Project 1/src/Controllers/inputReader.java
@@ -1,4 +1,8 @@
 package Controllers;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A class for comparing the strings being passed in
  * @author davidaghassi
@@ -6,7 +10,11 @@ package Controllers;
  */
 
 
-public class inputReader {
+public class InputReader {
+	//Used to compare the strings to each other
+	private HashMap<Character, Integer> base = new HashMap<Character, Integer>();
+	private HashMap<Character, Integer> compareToBase = new HashMap<Character, Integer>();
+	
 	
 	/**
 	 * Called to compare the two text boxes
@@ -15,10 +23,86 @@ public class inputReader {
 	 * @return True if the two values match
 	 */
 	public boolean readInput(String inputBoxLeft, String inputBoxRight){
-		boolean isTheSame = false;
+		//PUt the data in tables and clean it
+		readInData(inputBoxLeft, inputBoxRight);
 		
+		boolean isTheSame = false;
+		//If they aren't the same length, they can't be the same at all
+		if(base.size() != compareToBase.size()){
+			return isTheSame;
+		}
+		else{
+			/**
+			 * For each entry in the hash map, see if the other map contains that entry
+			 * If it does, we then check to see if the key values are the same
+			 * 
+			 */
+			for(Map.Entry<Character, Integer> entry : base.entrySet()){
+				if(compareToBase.containsKey(entry.getKey())){
+					if(compareToBase.get(entry.getKey()) == entry.getValue()){
+						isTheSame = true;
+					}
+					//If they don't have the same key value, we can stop there because they aren't the same
+					else{
+						break;
+					}
+				}
+				//If the entry doesn't contain the value at all, we can break and save time
+				else{
+					break;
+				}
+			}
+		}
 		return isTheSame;
 	}
 	
+	/**
+	 * Wrapper method that puts the data in hashmaps and cleans the strings
+	 * @param inputBoxLeft The data coming from the left input text box
+	 * @param inputBoxRight The data coming from the right input text box
+	 */
+	private void readInData(String inputBoxLeft, String inputBoxRight){
+		
+		//Clean the strings before use.
+		String baseString = cleanseString(inputBoxLeft);
+		String compareToBaseString = cleanseString(inputBoxRight);
+		
+		//Abstracted the population method for future maintence
+		populateHashMaps(baseString, base);
+		populateHashMaps(compareToBaseString, compareToBase);
+		
+	}
+	
+	/**
+	 * Creates the hash map where the data will be stored
+	 * The hashmap will add new data, and increment the value count of old data.
+	 * @param string The string to be broken into characters and put in the hashmap
+	 * @param table The hashmap to store it to
+	 */
+	private void populateHashMaps(String string, HashMap<Character, Integer> table){
+		for(int i = 0; i < string.length(); i++){
+			//If the character already exists
+			if(table.containsKey(string.charAt(i))){
+				int count = (Integer) table.get(string.charAt(i)) + 1;
+				table.put(string.charAt(i), count);
+			}
+			else{
+				table.put(string.charAt(i), 1);
+			}
+		}
+	}
+	
+	/**
+	 * Cleans the strings prior to use. Removes white space and puts it as lowercase
+	 * @param stringToBeCleansed
+	 * @return Returns the cleaned string.
+	 */
+	private String cleanseString(String stringToBeCleansed){
+		//Trim the strings and make them lower case
+		String cleansedString = stringToBeCleansed;
+		cleansedString.trim();
+		cleansedString.toLowerCase();
+		return cleansedString;
+	}
 
 }
--- 293 Programming Project 1/src/View/wordWindow.java
+++ 293 Programming Project 1/src/View/wordWindow.java
@@ -7,10 +7,20 @@ package View;
  * @author davidaghassi
  */
 
-import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.ParseException;
 
-public class wordWindow {
-	
+import javax.swing.*;
+import javax.swing.text.MaskFormatter;
+
+import Controllers.InputReader;
+
+public class WordWindow extends JFrame {
+	static JFormattedTextField leftTextField;
+	static JFormattedTextField rightTextField;
+	static JTextArea result = new JTextArea("Type in two strings to be compared");
 
 	/**
 	 * @param args
@@ -22,19 +32,81 @@ public class wordWindow {
 
 	
 	private static void createWindow(){
+		//Text fields
+		JButton submitButton;
+		
 		//Submit button
 		JFrame mainWindow = new JFrame();
 		//Sets the window size
-		mainWindow.setSize(500, 500);
+		mainWindow.setSize(500, 200);
 		//Title
 		mainWindow.setTitle("EECS 293 Programming Project 1");
 		//Not resizable
 		mainWindow.setResizable(false);
 		//Will close when the red button is clicked.
 		mainWindow.setDefaultCloseOperation(3);
+		
+		//Set the layout as a 2x3 grid
+		mainWindow.setLayout(new GridLayout(2, 3));
+		
+		//This formatter allows for up to a 10 character word.
+		//No spaces allowed.
+		MaskFormatter leftMask = null;
+		try {
+			leftMask = new MaskFormatter(generateFormat('L', 10));
+			leftMask.setInvalidCharacters("1234567890 ");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		MaskFormatter rightMask = null;
+		try {
+			rightMask = new MaskFormatter(generateFormat('L', 10));
+			rightMask.setInvalidCharacters("1234567890 ");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//Initialize components
+		leftTextField = new JFormattedTextField(leftMask);
+		rightTextField = new JFormattedTextField(rightMask);
+		submitButton = new JButton("Submit");
+		
+		submitButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+		    {
+		       InputReader reader = new InputReader();
+		       result.setText("These two strings are:");
+		       if(reader.readInput(leftTextField.getText(), rightTextField.getText())){
+		    	   result.append(" a match");
+		       }
+		       else{
+		    	   result.append(" not a match");
+		       }
+		    }
+		});
+		
+		//Add the components to the view
+		mainWindow.add(leftTextField);
+		mainWindow.add(rightTextField);
+		mainWindow.add(submitButton);
+		mainWindow.add(result);
+		
+		
 		//Makes window visible
 		mainWindow.setVisible(true);
 		
+	}
+
+	/**
+	 * Allows for a string with specified character amount to be created
+	 * @param with character to be put into the string
+	 * @param count number of characters to be put in
+	 * @return The formatted string with the length provided filled with the characters passed in.
+	 */
+	public static String generateFormat(char with, int count) {
+	    return String.format("%" + count + "s", "").replaceAll(" ", Character.toString(with));
 	}
 
 }
