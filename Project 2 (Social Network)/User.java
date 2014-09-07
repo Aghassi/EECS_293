0a936c1f459eb4759d067e76a6ad2270893b00e7
David <david.aghassi@gmail.com>
2014-09-07 14:59:53 -0400

    Added Project 2

---
+++ Project 2 (Social Network)/User.java
@@ -0,0 +1,31 @@
+/**
+* A class that represents a user object
+* David Aghassi (dsa28@case.edu)
+**/
+
+public class User{
+
+	public User(){
+		//creates a new user, no exceptions thrown
+
+	}
+
+	public boolean setID(String id){
+		//Marks the user as valid
+		//If valid do nothing
+		//Return true otherwise
+		//If null, throw nullPointerException
+	}
+
+	public String getID(){
+
+	}
+
+	public boolean isInvalid(){
+
+	}
+
+	public String toString(){
+
+	}
+}
