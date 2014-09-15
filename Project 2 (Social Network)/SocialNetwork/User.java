fdd2616c6ef5364b66fc38e2204e57a67bd1c9e9
David <david.aghassi@gmail.com>
2014-09-15 08:32:35 -0400

    Added new methods to the user class, did some slight refactoring.

--- Project 2 (Social Network)/SocialNetwork/User.java
+++ Project 2 (Social Network)/SocialNetwork/User.java
@@ -1,5 +1,7 @@
 package SocialNetwork;
 
+import java.security.InvalidParameterException;
+
 /**
 * A class that represents a user object
 * David Aghassi (dsa28@case.edu)
@@ -7,6 +9,9 @@ package SocialNetwork;
 
 public class User{
 	private String id;
+    private String firstName, lastName, middleName;
+    private String email;
+    private String phoneNumber;
 	private boolean isValidUser;
 
 	/**
@@ -20,7 +25,107 @@ public class User{
         }
 		this.id = id;
 		isValidUser = true;
+        firstName = null;
+        lastName = null;
+        middleName = null;
+        email = null;
+        phoneNumber = null;
 	}
+
+
+    /**
+     * Sets the first name of the user. Throws exceptions if the user is invalid
+     * or if the input is null
+     * @param name The name to be set to the first name
+     * @return The user
+     * @throws Exception
+     */
+    public User setFirstName(String name) throws Exception{
+        checkForExceptions(name);
+        firstName = name;
+        return this;
+    }
+
+    /**
+     * Gets the first name of the user
+     * @return The user's first name
+     */
+    public String getFirstName(){
+        return firstName;
+    }
+
+    /**
+     * Sets the last name of the user. Throws exceptions if the user is invalid
+     * or if the input is null
+     * @param name The name to be set to the last name
+     * @return The user
+     * @throws Exception
+     */
+    public User setLastName(String name) throws Exception{
+        checkForExceptions(name);
+        lastName = name;
+        return this;
+    }
+
+    /**
+     * Gets the last name of the user
+     * @return The user's last name
+     */
+    public String getLastName(){
+        return lastName;
+    }
+
+    /**
+     * Sets the middle name of the user. Throws exceptions if the user is invalid
+     * or if the input is null
+     * @param name The name to be set to the middle name
+     * @return The user
+     * @throws Exception
+     */
+    public User setMiddleName(String name) throws Exception{
+        checkForExceptions(name);
+        middleName = name;
+        return this;
+    }
+
+    /**
+     * Gets the middle name of the user
+     * @return The user's middle name
+     */
+    public String getMiddleName(){
+        return middleName;
+    }
+
+    /**
+     * Sets the email for the current user. Will throw exceptions if the email doesn't contain @,
+     * if the input is null, or if the user is not valid.
+     * @param emailAddress The email being passed in
+     * @return The current user
+     * @throws Exception
+     */
+    public User setEmailAddress(String emailAddress) throws Exception{
+        checkForExceptions(emailAddress);
+        if(!emailAddress.contains('@')){
+            throw new InvalidParameterException("Email did not containt @ symbold!");
+        }
+        email = emailAddress;
+        return this;
+    }
+
+    /**
+     * Gets the current email of the user
+     * @return The current email of the user.
+     */
+    public String getEmailAddress(){
+        return email;
+    }
+
+    public User setPhoneNumber(String number){
+        //How many numbers is a phone number, are we accounting for world wide?
+        checkForExceptions(number);
+        phoneNumber = number;
+        return this;
+    }
 
 	/**
 	* Sets the id of the user
@@ -31,14 +136,12 @@ public class User{
         if(id == null){
             throw new UninitializedObjectException("Please create a valid user id");
         }
-        else {
-            if (!this.isValidUser) {
-                this.isValidUser = true;
-                return true;
-            } else {
-                return false;
-            }
+        if (!this.isValidUser) {
+            this.isValidUser = true;
+            return true;
         }
+
+        return false;
 	}
 
 	/**
@@ -49,9 +152,8 @@ public class User{
 		if(!isValid()){
 			return null;
 		}
-		else{
-			return this.id;
-		}
+
+        return this.id;
 	}
 
 	/**
@@ -74,4 +176,13 @@ public class User{
 			return "SocialNetwork.User: " + this.id;
 		}
 	}
+
+    private void checkForExceptions(String string){
+        if (string.equals(null)){
+            throw new NullPointerException();
+        }
+        if (!isValid()){
+            throw new UninitializedObjectException();
+        }
+    }
 }
